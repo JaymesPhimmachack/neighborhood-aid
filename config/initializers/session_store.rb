6a3738f060b3a7734a58e9519f8c888ea33a82b6
Jaymes Phimmachack <phimmachack.jaymes@gmail.com>
2020-11-06 20:17:12 -0500

    Fix cors issue

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,6 @@
 if Rails.env == "production"
-	Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "https://jp-neighborhood-aid.herokuapp.com/"
+	Rails.application.config.session_store :cookie_store, key: "_neighborhood_aid_app", domain: "https://jp-neighborhood-aid.herokuapp.com/"
 else
-	Rails.application.config.session_store :cookie_store, key: "_authentication_app"
+	Rails.application.config.session_store :cookie_store, key: "_neighborhood_aid_app"
 end	
+
