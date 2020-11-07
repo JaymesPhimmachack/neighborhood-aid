7c3ed3881d19bf94983c65534eafda68ef096677
Jaymes Phimmachack <phimmachack.jaymes@gmail.com>
2020-11-07 17:40:36 -0500

    Change websocket url

--- app/javascript/packs/src/cable.js
+++ app/javascript/packs/src/cable.js
@@ -1,5 +1,5 @@
 import { createConsumer } from "@rails/actioncable";
-const URL = "ws://jp-neighborhood-aid.herokuapp.com/cable";
+const URL = "wss://jp-neighborhood-aid.herokuapp.com/cable";
 const consumer = createConsumer(URL);
 
 export default consumer;
