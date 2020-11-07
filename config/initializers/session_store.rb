0a17707380b5f3493c13ef97957a11aa65c8a282
Jaymes Phimmachack <phimmachack.jaymes@gmail.com>
2020-11-07 17:10:04 -0500

    Change domain name

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-	Rails.application.config.session_store :cookie_store, key: "_neighborhood_aid_app", domain: "https://jp-neighborhood-aid.herokuapp.com/"
+	Rails.application.config.session_store :cookie_store, key: "_neighborhood_aid_app", domain: "jp-neighborhood-aid.herokuapp.com"
 else
 	Rails.application.config.session_store :cookie_store, key: "_neighborhood_aid_app"
 end	
